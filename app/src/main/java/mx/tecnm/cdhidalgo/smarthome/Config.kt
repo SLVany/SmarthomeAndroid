84f4db1bdc5340b5d6d64f1025db3cc191198504
SLVany <iovanyblancas@gmail.com>
2023-04-20 21:41:45 -0600

    Login y Crud listo :D

---
+++ app/src/main/java/mx/tecnm/cdhidalgo/smarthome/Config.kt
@@ -0,0 +1,9 @@
+package mx.tecnm.cdhidalgo.smarthome
+
+import java.net.URL
+
+class Config {
+    companion object {
+        val URL:String = "http://192.168.0.11/"
+    }
+}
---
+++ app/src/main/java/mx/tecnm/cdhidalgo/smarthome/MyListener.kt
@@ -0,0 +1,6 @@
+package mx.tecnm.cdhidalgo.smarthome
+
+interface MyListener {
+    fun onClickEdit(posicion: Int)
+    fun onClickDel(posicion: Int)
+}
